9053d439e8272d402b877bb4166fdf7449ded9af
abby <diya811125@gmail.com>
2021-06-26 15:21:56 +0800

    修改pin顏色

--- js/custom.js
+++ js/custom.js
@@ -55,7 +55,7 @@ xhr.onload = function () {
 		center: [data[0]["latitude"], data[0]["longitude"]],
 		zoom: 18
 	});
-	
+
 	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
